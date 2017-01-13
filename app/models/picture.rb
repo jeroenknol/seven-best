b47626b5526b67483d66aefa6b0f53e15db4b033
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-01-13 17:05:22 +0100

    Add Validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,9 @@
 class Picture < ApplicationRecord
   belongs_to :user
+
+  validates :image_url, presence: true,
+                        length: { maximum: 255 }
+  validates :caption,   presence: true,
+                        length: { maximum: 140 }
+  validates :user_id,   presence: true                      
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
-  has_many :pictures
+  has_many :pictures, dependent: :destroy
+
+  before_save :downcase_email
+
+  validates :first_name,  presence: true,
+                          length: { minimum: 2, maximum: 70 }
+  validates :last_name,   presence: true,
+                          length: { minimum: 2, maximum: 70 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email,       presence: true,
+                          uniqueness: { case_sensitive: false },
+                          length: { maximum: 255 },
+                          format: { with: VALID_EMAIL_REGEX }
+
+  private
+    def downcase_email
+      email.downcase!
+    end
 end
